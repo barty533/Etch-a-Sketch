701bd2627578addfdad543d673cfd1f9b2dfec74
barty533 <imamotherlover37@gmail.com>
2024-10-03 20:52:19 +0200

    Add new button for changing the grid size

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const content = document.querySelector(".main");
+
+
+const header = document.querySelector(".header");
+
+const content = document.createElement("div");
+content.className = "main";
+document.body.appendChild(content);
+
+
+const btn = document.createElement("button"); 
+header.appendChild(btn);
+btn.className = "gridBtn";
+btn.textContent = "Change grid size";
+
 
 
 
 function createDivs(num){
 
+  
+        
+    
+    
+     
     for(let i=0; i<num; i++)
     {
         const container = document.createElement("div");
@@ -15,14 +33,57 @@ function createDivs(num){
             if(MouseEvent) 
             div.addEventListener("mousedown", function()
             { div.classList.add("active");});  
+            
             container.append(div);
-        }
-      
+            
+            
+
+
+            
+        }   
     }
+
 
 }
 
 createDivs(16);
+ 
+btn.addEventListener("click", function(){
+    x = prompt("Enter the size of the grid");
+    
+    if(x<=100)
+    {
+    content.replaceChildren();
+    createDivs(x);
+    }
+    else{
+        content.replaceChildren();
+        const warning = document.createElement("div")
+        warning.className = "warning";
+        warning.textContent = "Maximum grid size is 100, try something else."
+        content.appendChild(warning);
+    }
+    
+
+} );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
